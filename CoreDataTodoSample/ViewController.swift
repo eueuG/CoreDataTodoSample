2cd517a5f9319065e18c7805b3269ecef7086908
rinetrous <rinellie1228@icloud.com>
2021-06-06 21:46:18 +0900

    CoreData func実装

--- CoreDataTodoSample/ViewController.swift
+++ CoreDataTodoSample/ViewController.swift
@@ -9,25 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Core Data To Do List"
     }
 
+    
+    
+    
+    
+    // Core Data
+
     func getAllItems() {
-        
+        do {
+            let items = try context.fetch(TodoListItem.fetchRequest())
+        }
+        catch {
+            // error
+        }
     }
 
     func createItem(name: String) {
+        let newItem = TodoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
         
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
     }
     
     func deleteItem(item: TodoListItem) {
-        
+        context.delete(item)
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
     }
     
-    func updateItem(item: TodoListItem) {
-        
+    func updateItem(item: TodoListItem, newName: String) {
+        item.name = newName
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
     }
 }
 
