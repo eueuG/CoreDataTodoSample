622df8a3a98d08cab417ea6dbf473d8795acc0be
rinetrous <rinellie1228@icloud.com>
2021-06-06 23:26:23 +0900

    Edit, Delete機能実装

--- CoreDataTodoSample/ViewController.swift
+++ CoreDataTodoSample/ViewController.swift
@@ -56,8 +56,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        
+        let sheet = UIAlertController(title: "Edit", message: nil, preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "Edit", style: .default, handler: { _ in
+            
+            let alert = UIAlertController(title: "Edit Item", message: "Edit your Item", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.name
+            alert.addAction(UIAlertAction(title: "Save", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newName = field.text, !newName.isEmpty else {
+                    return
+                }
+                
+                self?.updateItem(item: item, newName: newName)
+            }))
+            
+            self.present(alert, animated: true)
+        }))
+        sheet.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        self.present(sheet, animated: true)
+    }
+    
+    
     // Core Data
-
     func getAllItems() {
         do {
             models = try context.fetch(TodoListItem.fetchRequest())
@@ -89,6 +116,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         context.delete(item)
         do {
             try context.save()
+            getAllItems()
         }
         catch {
             
@@ -99,6 +127,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         item.name = newName
         do {
             try context.save()
+            getAllItems()
         }
         catch {
             
